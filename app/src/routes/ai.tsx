b2b58a2509d5cb1e82a72b700745d850f2d7b0e2
Debbal Lotfi <debbal.lotfi.dev@gmail.com>
2026-04-01 23:06:16 +0100

    update

--- app/src/routes/ai.tsx
+++ app/src/routes/ai.tsx
@@ -3,8 +3,6 @@ import RequiredConnectionProvider from "../provider/required-connection-provider
 import RequiredGeminiProvider from "../provider/required-gemini-provider";
 import AIDasboard from "../components/ai/ai-dashboard";
 
-// AIzaSyAhepEarYPcAGN89iLFKSQNAq3IQIJRL14
-
 export const Route = createFileRoute("/ai")({
   component: RouteComponent,
 });
